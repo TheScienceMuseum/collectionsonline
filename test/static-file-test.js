595be40b999a578cd141df08fd1968ceb9f045e6
Danielwhyte <danwhy@gmail.com>
2016-06-15 14:40:14 +0100

    updates tests to use es6 functions/const #28

--- test/static-file-test.js
+++ test/static-file-test.js
@@ -3,24 +3,24 @@ const fs = require('fs');
 const jsFile = fs.readFileSync('public/bundle.js');
 const cssFile = fs.readFileSync('public/bundle.css');
 
-testWithServer('Javascript Files served correctly', function (t, server) {
+testWithServer('Javascript Files served correctly', (t, server) => {
   t.plan(1);
 
-  var jsRequest = {
+  const jsRequest = {
     method: 'GET',
     url: '/bundle.js'
   };
 
-  server.inject(jsRequest, function (res) {
+  server.inject(jsRequest, (res) => {
     t.equal(res.payload.toString('utf-8'), jsFile.toString(), 'JS File should be served');
     t.end();
   });
 });
 
-testWithServer('CSS Files served correctly', function (t, server) {
+testWithServer('CSS Files served correctly', (t, server) => {
   t.plan(1);
 
-  var cssRequest = {
+  const cssRequest = {
     method: 'GET',
     url: '/bundle.css'
   };
@@ -31,15 +31,15 @@ testWithServer('CSS Files served correctly', function (t, server) {
   });
 });
 
-testWithServer('Non Existent Files', function (t, server) {
+testWithServer('Non Existent Files', (t, server) => {
   t.plan(1);
 
-  var badRequest = {
+  const badRequest = {
     method: 'GET',
     url: '/notfound.js'
   };
 
-  server.inject(badRequest, function (res) {
+  server.inject(badRequest, (res) => {
     t.equal(res.statusCode, 404, 'Non-existent file should return 404');
     t.end();
   });
