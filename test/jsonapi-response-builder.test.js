39c17f0b39b4d341abb9829b23305bda2ce26913
Danielwhyte <danwhy@gmail.com>
2016-06-20 16:32:33 +0100

    adds tests for json response builder

---
+++ test/jsonapi-response-builder.test.js
@@ -0,0 +1,13 @@
+var test = require('tape');
+const buildJSONResponse = require('../lib/jsonapi-response.js');
+const exampleGETResponse = require('./fixtures/example-get-response.json');
+
+test('Building the JSON Response', function (t) {
+  t.plan();
+  const JSONAPIResponse = JSON.parse(buildJSONResponse(exampleGETResponse));
+
+  t.ok(JSONAPIResponse.data, 'Response contains data field' + JSONAPIResponse.data);
+  t.ok(JSONAPIResponse.relationships, 'Response contains relationships field');
+  t.ok(JSONAPIResponse.links, 'Response contains links field');
+  t.end();
+});
