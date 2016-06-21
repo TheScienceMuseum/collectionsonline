00a514916159a2d96fd6cc6dce1450288ee3a847
Danielwhyte <danwhy@gmail.com>
2016-06-21 16:14:01 +0100

    fixes included field in json builder

--- test/jsonapi-response-builder.test.js
+++ test/jsonapi-response-builder.test.js
@@ -4,8 +4,9 @@ const buildJSONResponse = require('../lib/jsonapi-response.js');
 const exampleGETResponse = require('./fixtures/example-get-response.json');
 
 test('Building the JSON Response', function (t) {
-  t.plan(11);
+  t.plan(13);
   const JSONAPIResponse = buildJSONResponse(exampleGETResponse, config);
+  const terms = JSONAPIResponse.included.filter(el => el.type === 'term');
 
   t.ok(JSONAPIResponse.data, 'Response contains data field');
   t.equal(JSONAPIResponse.data.type, 'objects', 'Data field contains correct type');
@@ -23,5 +24,8 @@ test('Building the JSON Response', function (t) {
   t.equal(JSONAPIResponse.links.self, config.rootUrl + '/objects/smgc-objects-8245103', 'Response contains links field');
 
   t.ok(JSONAPIResponse.included, 'Response contains included field');
+  t.equal(JSONAPIResponse.included.length, 4, 'Included field contains correct number of resources');
+
+  t.equal(terms.length, 3, 'Included field contains correct number of terms');
   t.end();
 });
