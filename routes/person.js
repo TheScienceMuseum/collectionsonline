20947c647729306da921fd865a41d51b69548887
Danielwhyte <danwhy@gmail.com>
2016-06-22 17:06:03 +0100

    transforms initial data for person template #43

--- routes/person.js
+++ routes/person.js
@@ -1,8 +1,7 @@
-const fs = require('fs');
 const Boom = require('boom');
-const exampleData = JSON.parse(fs.readFileSync('./src/data/person.json'));
 const buildJSONResponse = require('../lib/jsonapi-response');
 const TypeMapping = require('../lib/type-mapping');
+const JSONToHTML = require('../lib/transforms/json-to-html-data.js');
 
 module.exports = ({ elastic, config }) => ({
   method: 'GET',
@@ -14,11 +13,23 @@ module.exports = ({ elastic, config }) => ({
         mediaTypes: {
           'text/html' (request, reply) {
             const data = {
-              page: 'person',
-              title: 'Charles Babbage'
+              page: 'person'
             };
 
-            reply.view('person', Object.assign(exampleData, data));
+            elastic.get({index: 'smg', type: 'agent', id: TypeMapping.toInternal(request.params.id)}, (err, result) => {
+              if (err) {
+                return reply(err);
+              }
+
+              if (!result) {
+                return reply.view('noresults');
+              }
+
+              const JSONData = buildJSONResponse(result, config);
+              const HTMLData = JSONToHTML(JSONData);
+
+              reply.view('person', Object.assign(HTMLData, data));
+            });
           },
           'application/vnd.api+json' (req, reply) {
             elastic.get({index: 'smg', type: 'agent', id: TypeMapping.toInternal(req.params.id)}, (err, result) => {
