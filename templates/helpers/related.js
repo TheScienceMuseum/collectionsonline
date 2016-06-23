d25100043d208e226e12e5c2dacfbdd963169280
Danielwhyte <danwhy@gmail.com>
2016-06-23 17:10:46 +0100

    moves description formatting into json-to-html #43

--- templates/helpers/related.js
+++ templates/helpers/related.js
@@ -3,7 +3,7 @@ const Handlebars = require('handlebars');
 module.exports = (relation) => {
   const url = relation.links ? relation.links.self : '#';
   const name = relation.attributes.summary_title;
-  let role;
+  var role;
 
   if (relation.attributes && relation.attributes.role) {
     role = `(${relation.attributes.role})`;
