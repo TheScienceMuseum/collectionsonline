7fd4e06d50f3c1da784392391ea7515fa73b69ab
Danielwhyte <danwhy@gmail.com>
2016-07-04 10:53:44 +0100

    splits and capitalises occupation(s) #106

--- test/client/routes/person.clienttest.js
+++ test/client/routes/person.clienttest.js
@@ -4,7 +4,7 @@ module.exports = {
       .url('http://localhost:8000/people/smgc-people-36993')
       .waitForElementVisible('body', 1000)
       .assert.containsText('.record-top__title', 'Babbage')
-      .assert.containsText('.fact-occupation', 'mathematician')
+      .assert.containsText('.fact-occupation', 'Mathematician')
       .assert.containsText('.fact-Nationality', 'English')
       .assert.containsText('.fact-born-in', 'Southwark')
       .assert.containsText('.record-top__date', '1791 - 1871')
