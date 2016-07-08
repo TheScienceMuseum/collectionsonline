ee24d6be113420f71218c901186f6486f55e7c3f
Danielwhyte <danwhy@gmail.com>
2016-07-08 10:10:34 +0100

    checkboxes stay checked/filters stay open on page reload #146

--- public/hide.js
+++ public/hide.js
@@ -1,4 +1,6 @@
 // Hides filterpanel by default if javascript is enabled
-document.getElementById('searchresults').className = 'row searchresults';
-document.getElementById('filtercolumn').className = 'filtercolumn';
-document.getElementById('control--filters').className = 'control control--filters';
+if (!document.querySelector('input.filter__box:checked')) {
+  document.getElementById('searchresults').className = 'row searchresults';
+  document.getElementById('filtercolumn').className = 'filtercolumn';
+  document.getElementById('control--filters').className = 'control control--filters';
+}
