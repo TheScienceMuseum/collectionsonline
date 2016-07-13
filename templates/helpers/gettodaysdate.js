376e3cbd8eb12ac7eaf627cecf32a8cf32a4f688
Danielwhyte <danwhy@gmail.com>
2016-07-13 19:53:13 +0100

    simplifies date function #173

--- templates/helpers/gettodaysdate.js
+++ templates/helpers/gettodaysdate.js
@@ -1,19 +1,4 @@
-module.exports = () => {
-  var months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ];
-  var today = new Date();
-
-  return today.getDate() + ' ' + months[today.getMonth()] + ' ' + today.getFullYear();
+module.exports = function () {
+  var options = { day: 'numeric', month: 'long', year: 'numeric' };
+  return new Date().toLocaleDateString('en-GB', options);
 };
