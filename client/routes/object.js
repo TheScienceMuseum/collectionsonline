ed62293dd5092b804a814d0b7ade0ace5ce2ce2f
SimonLab <simon.labondance@gmail.com>
2016-07-29 14:42:26 +0100

    fetch data of objects, #222

--- client/routes/object.js
+++ client/routes/object.js
@@ -1,43 +1,31 @@
-var fetch = require('fetch-ponyfill')();
 var Templates = require('../templates');
-var JSONToHTML = require('../../lib/transforms/json-to-html-data');
 var exampleData = require('../../src/data/object.json');
 var initJqueryComp = require('../lib/init-jquery-components.js');
+var getDataItem = require('../lib/get-data-item');
 
 module.exports = function (page) {
-  page('/objects/:id', enter);
+  page('/objects/:id', load, render);
+};
 
-  function enter (ctx) {
-    var pageEl = document.getElementsByTagName('main')[0];
-
-    var id = ctx.params.id;
-    var url = '/objects/' + id;
-
+function load (ctx, next) {
+  if (!ctx.isInitialRender) {
     var opts = {
       headers: { Accept: 'application/vnd.api+json' }
     };
-
-    fetch(url, opts)
-    .then(function (res) {
-      if (res.ok) {
-        return res.json();
-      } else {
-        return Promise.reject(new Error(res.status + ' Failed to fetch results'));
-      }
-    })
-    .then(function (json) {
-      if (json.errors) return Promise.reject(json.errors[0]);
-      var data = JSONToHTML(json);
-      data.slides = exampleData.slides;
-      pageEl.innerHTML = Templates['objects'](data);
-      document.getElementsByTagName('title')[0].textContent = data.titlePage;
-      window.scrollTo(0, 0);
-    })
-    .then(function () {
-      initJqueryComp();
-    })
-    .catch(function (err) {
-      console.error('Failed to find object', err);
+    var id = ctx.params.id;
+    var url = '/objects/' + id;
+    getDataItem(url, opts, function (data) {
+      ctx.state.data = data;
+      next();
     });
   }
-};
+}
+
+function render (ctx, next) {
+  var pageEl = document.getElementsByTagName('main')[0];
+  ctx.state.data.slides = exampleData.slides;
+  pageEl.innerHTML = Templates['objects'](ctx.state.data);
+  document.getElementsByTagName('title')[0].textContent = ctx.state.data.titlePage;
+  window.scrollTo(0, 0);
+  initJqueryComp();
+}
