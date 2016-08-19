b4a33c60b98d6c3ec2a18fb83aa8516cdce8ea8c
Alan Shaw <alan@tableflip.io>
2016-08-19 11:11:34 +0100

    Guard aginst infinite loop

--- routes/sitemap.js
+++ routes/sitemap.js
@@ -1,7 +1,13 @@
+const Path = require('path');
+
 module.exports = (config) => ({
   method: 'GET',
   path: '/sitemap.xml',
   handler: (request, reply) => {
+    if (!config.sitemapUrl || config.sitemapUrl === config.rootUrl) {
+      return reply.file(Path.join(__dirname, '..', 'public', 'sitemap.xml'));
+    }
+
     reply.proxy({ uri: `${config.sitemapUrl}/sitemap.xml`, passThrough: true });
   },
   config: {
