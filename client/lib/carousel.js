b4d7521355b6b33c7b1623e824607d8040f0cd5f
Danielwhyte <danwhy@gmail.com>
2016-09-01 15:47:14 +0100

    adds thumbnails to carousel #183

--- client/lib/carousel.js
+++ client/lib/carousel.js
@@ -2,11 +2,14 @@
 var Carousel = require('carousel-js');
 
 module.exports = () => {
+  var thumbnails = document.getElementsByClassName('carousel-thumb');
+
   var carousel = new Carousel({
     panels: document.getElementsByClassName('carousel-image'),
     panelActiveClass: 'carousel-image-active',
     leftArrow: document.getElementById('left-button'),
     rightArrow: document.getElementById('right-button'),
-    infinite: true
+    infinite: true,
+    thumbnails: thumbnails
   });
 };
