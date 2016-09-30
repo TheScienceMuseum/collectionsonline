63f2eeeafc940074d117cf90b4124ca41cb1e889
Danielwhyte <danwhy@gmail.com>
2016-09-30 10:40:01 +0100

    adds tests for material filter #401

--- test/search-objects-filters.test.js
+++ test/search-objects-filters.test.js
@@ -33,9 +33,44 @@ testWithServer(file + 'Should accept params in filter[PARAM_NAME] format for obj
     method: 'GET',
     url: '/search/objects?' + QueryString.stringify({
       q: 'rocket',
+      'filter[categories]': 'Locomotives and Rolling Stock',
       'filter[date[to]]': '2000',
       'filter[type]': 'steam locomotive',
       'filter[user]': 'Liverpool & Manchester Railway'
+    }),
+    headers: { Accept: 'text/html' }
+  };
+
+  ctx.server.inject(htmlRequest, (res) => {
+    t.equal(res.statusCode, 200, 'Status code was as expected');
+    t.end();
+  });
+});
+
+testWithServer(file + 'Should accept params in filter[PARAM_NAME] format for objects type', {}, (t, ctx) => {
+  t.plan(1);
+
+  const htmlRequest = {
+    method: 'GET',
+    url: '/search/objects?' + QueryString.stringify({
+      'filter[material]': 'glass'
+    }),
+    headers: { Accept: 'text/html' }
+  };
+
+  ctx.server.inject(htmlRequest, (res) => {
+    t.equal(res.statusCode, 200, 'Status code was as expected');
+    t.end();
+  });
+});
+
+testWithServer(file + 'Should accept params in filter[PARAM_NAME] format for objects type', {}, (t, ctx) => {
+  t.plan(1);
+
+  const htmlRequest = {
+    method: 'GET',
+    url: '/search/objects?' + QueryString.stringify({
+      'filter[material]': ['glass', 'cork']
     }),
     headers: { Accept: 'text/html' }
   };
