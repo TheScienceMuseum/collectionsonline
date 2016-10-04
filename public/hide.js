cda05fd293874ca0a1f65f47eff2e639732eaa42
SimonLab <simon.labondance@gmail.com>
2016-10-04 18:02:34 +0100

    hide filters if no results, #421

--- public/hide.js
+++ public/hide.js
@@ -2,7 +2,11 @@
 if (!document.querySelector('input.filter__box:checked')) {
   document.getElementById('searchresults').className = 'row searchresults';
   document.getElementById('filtercolumn').className = 'filtercolumn';
-  document.getElementById('control--filters').className = 'control control--filters';
-  document.getElementById('fb').className = 'control__button';
+  // control filters is not displayed if there is no result and no filter selected
+  var controlFilters = document.getElementById('control--filters');
+  if (controlFilters) {
+    controlFilters.className = 'control control--filters';
+    document.getElementById('fb').className = 'control__button';
+  }
   document.querySelector('button.filterpanel__button').style.display = 'none';
 }
