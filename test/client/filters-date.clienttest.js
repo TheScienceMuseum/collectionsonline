627ef20a1dfc2e3720300c0956d99d0e37ea6133
SimonLab <simon.labondance@gmail.com>
2016-10-12 22:31:58 +0100

    update timeout time for nightwatch test, #244

--- test/client/filters-date.clienttest.js
+++ test/client/filters-date.clienttest.js
@@ -3,11 +3,11 @@ module.exports = {
     browser
     .url('http://localhost:8000/search/people?q=ada')
     .waitForElementVisible('body', 1000)
-    .waitForElementVisible('.filter[data-filter="Dates"] a', 1000)
+    .waitForElementVisible('.filter[data-filter="Dates"] a', 7000)
     .click('.filter[data-filter="Dates"] a')
     .setValue('input[name="filter[birth[date]]"]', '1700')
     .click('input[name="filter[death[date]]"]')
-    .pause(1000)
+    .pause(5000)
     .assert.urlEquals('http://localhost:8000/search/people?q=ada&filter%5Bbirth%5Bdate%5D%5D=1700&page%5Bsize%5D=50&page%5Btype%5D=search')
     .end();
   }
