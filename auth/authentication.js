f3dabcf094dfc7daf24424aaa10e6bd479a04548
SimonLab <simon.labondance@gmail.com>
2016-10-28 16:39:19 +0100

    update to hapi 15, #398

--- auth/authentication.js
+++ auth/authentication.js
@@ -1,6 +1,9 @@
 const validateToken = require('./validate-token');
 const config = require('../config');
 exports.register = function (server, options, next) {
+  server.state('token', {
+    isSecure: false
+  });
   server.auth.strategy('jwt', 'jwt', false,
     {
       key: config.JWT_SECRET,
