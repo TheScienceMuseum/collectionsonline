8bed66526190b4ea99d6d52b316f9c0329ee438d
SimonLab <simon.labondance@gmail.com>
2016-11-22 22:28:36 +0000

    convert date of multimedia.sort to timestamp for comparaison, #499

--- test/sort-images.test.js
+++ test/sort-images.test.js
@@ -9,12 +9,14 @@ test(file + 'Sort a list of images by priority and date upload(sort pririty)', (
     {priority: 2, sort: '2015-06-07 13:10:41.0'},
     {priority: 1, sort: '2016-06-07 13:10:41.0'},
     {priority: 2, sort: '2015-06-07 16:10:41.0'},
-    {priority: 1, sort: '2015-06-07 10:10:40.0'}
+    {priority: 1, sort: '2015-06-07 10:10:40.0'},
+    {priority: 1, sort: '2016-07-07'}
   ];
 
   const expected = [
    {priority: 2, sort: '2015-06-07 16:10:41.0'},
    {priority: 2, sort: '2015-06-07 13:10:41.0'},
+   {priority: 1, sort: '2016-07-07'},
    {priority: 1, sort: '2016-06-07 13:10:41.0'},
    {priority: 1, sort: '2015-06-07 13:10:41.0'},
    {priority: 1, sort: '2015-06-07 10:10:40.0'}
