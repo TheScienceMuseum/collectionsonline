7289e639d2f3b5c66cb9d0c41882aa4ab0759688
Danielwhyte <danwhy@gmail.com>
2016-11-28 13:14:39 +0000

    fixes tests for new index

--- test/client/routes/person.clienttest.js
+++ test/client/routes/person.clienttest.js
@@ -4,7 +4,6 @@ module.exports = {
       .url('http://localhost:8000/people/cp36993')
       .waitForElementVisible('body', 1000)
       .assert.containsText('.record-top__title', 'Charles Babbage')
-      .assert.containsText('.fact-occupation', 'Computer pioneer, Mathematician')
       .assert.containsText('.fact-Nationality', 'English')
       .assert.containsText('.fact-born-in', 'Southwark')
       .assert.containsText('.record-top__date', '1791 - 1871')
