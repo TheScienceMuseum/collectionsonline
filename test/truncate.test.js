8f197aa02ec7e346cc1372d0422b65c285c9f123
SimonLab <simon.labondance@gmail.com>
2016-11-30 11:03:51 +0000

    add test for truncate, #605

--- test/truncate.test.js
+++ test/truncate.test.js
@@ -4,5 +4,6 @@ const truncate = require('../lib/helpers/search-results-to-template-data/truncat
 test('Should convert to external types', (t) => {
   t.equal(truncate('hello there', 5), 'hello...', 'truncate with maxChar 5');
   t.equal(truncate('hello there', 50), 'hello there', 'return all value if maxChar > value.length');
+  t.equal(truncate('', 50), undefined, 'return undefined when the value is empty or not defined');
   t.end();
 });
