3e2cac98c41e653d44738b47396e423936b5b2cd
Danielwhyte <danwhy@gmail.com>
2016-12-05 10:43:28 +0000

    makes image download work in firefox #651

--- client/lib/download-image.js
+++ client/lib/download-image.js
@@ -2,13 +2,16 @@ module.exports = () => {
   var download = document.querySelectorAll('.download-image');
   var a = document.createElement('a');
   if (download) {
-    Array.prototype.slice.call(download).forEach(e => e.addEventListener('submit', event => {
-      if (typeof a.download !== undefined) {
-        event.preventDefault();
-        a.href = e.action;
-        a.download = 'image.jpeg';
-        a.click();
-      }
-    }));
+    Array.prototype.slice.call(download).forEach(e => {
+      e.addEventListener('submit', event => {
+        if (typeof a.download !== undefined) {
+          event.preventDefault();
+          a.href = e.action;
+          a.download = 'image.jpeg';
+          document.body.appendChild(a);
+          a.click();
+        }
+      });
+    });
   }
 };
