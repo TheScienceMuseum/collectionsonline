8c173a1b8fd32e12fe9da3dc4720536d01205910
SimonLab <simon.labondance@gmail.com>
2016-12-09 11:54:46 +0000

    fix linter

--- test/get-primary-value.test.js
+++ test/get-primary-value.test.js
@@ -2,7 +2,6 @@ const test = require('tape');
 const getPrimaryValue = require('../lib/get-primary-value.js');
 
 test('Get primary value of a string return the sting', (t) => {
-
   t.equal(getPrimaryValue('hello'), 'hello', 'get primary value of hello is hello');
   t.end();
 });
