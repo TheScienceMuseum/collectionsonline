5d31da85c10bf1316b3f2c2336cbacaa7d95630a
Danielwhyte <danwhy@gmail.com>
2017-02-06 15:56:03 +0000

    allows clicking images to open zooms #763

--- client/lib/osd-listener.js
+++ client/lib/osd-listener.js
@@ -1,19 +1,20 @@
 var openseadragon = require('../lib/openseadragon');
 
 module.exports = (ctx) => {
-  var osd = document.getElementsByClassName('osd__toolbar');
-  if (osd) {
-    Array.prototype.slice.call(osd).forEach(function (el) {
-      el.addEventListener('click', function (e) {
-        if (!ctx.viewer) {
-          openseadragon.init(ctx);
-        }
-        if (e.target.id === 'osd-fullpage') {
-          ctx.viewer.setFullScreen(true);
-        } else if (e.target.id === 'osd-home') {
-          openseadragon.quit(ctx);
-        }
-      });
+  var osd = Array.prototype.slice.call(document.getElementsByClassName('osd__toolbar')) || [];
+  var singleImages = Array.prototype.slice.call(document.getElementsByClassName('single_image')) || [];
+  var selectedImages = Array.prototype.slice.call(document.getElementsByClassName('flickity-viewport')) || [];
+  var images = osd.concat(singleImages).concat(selectedImages);
+
+  images.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      if (!ctx.viewer) {
+        openseadragon.init(ctx);
+      }
+      ctx.viewer.setFullScreen(true);
+      if (e.target.id === 'osd-home') {
+        openseadragon.quit(ctx);
+      }
     });
-  }
+  });
 };
