a2a959262831365a1c43eb736d978f2353189ba5
Danielwhyte <danwhy@gmail.com>
2017-02-07 12:47:19 +0000

    fixes date and extent fields for archive pages #744

--- test/transforms/json-to-html-doc.test.js
+++ test/transforms/json-to-html-doc.test.js
@@ -22,7 +22,7 @@ test('Data fields should have correct values', function (t) {
   }, 'Transform did not throw error');
   t.equal(HTMLData.title, 'The Babbage Papers', 'title should be correct');
   t.equal(HTMLData.type, 'documents', 'type should be correct');
-  t.equal(HTMLData.fact.length, 1, 'facts should not be empty');
+  t.equal(HTMLData.fact.length, 2, 'facts should not be empty');
   t.equal(HTMLData.related.documents.length, 8, 'Data should contain eight related documents');
   t.ok(HTMLData.description, 'Data should contain a description');
   t.end();
