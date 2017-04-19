3026d8c2fb586329d306acb4f780548a9849c265
Danielwhyte <danwhy@gmail.com>
2017-04-19 18:26:35 +0100

    makes lowercase urls work #798

--- templates/helpers/isselected.js
+++ templates/helpers/isselected.js
@@ -1,5 +1,17 @@
 module.exports = (selected, facet, name) => {
-  if (selected[facet] && selected[facet][name]) {
+  var isSelected = false;
+
+  if (selected[facet] && typeof name === 'string') {
+    for (var filterName in selected[facet]) {
+      if (filterName.toLowerCase() === name.toLowerCase()) {
+        isSelected = true;
+      }
+    }
+  } else if (selected[facet] && name && typeof name === 'boolean') {
+    isSelected = true;
+  }
+
+  if (isSelected) {
     return 'checked';
   } else {
     return '';
