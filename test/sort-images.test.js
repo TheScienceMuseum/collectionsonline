054ecf40efe459752e68493a59d2be88132c5fc8
Sam Houston <shouston3@me.com>
2017-07-04 15:40:47 +0100

    improving coverage in sort-images and removing unused helper

--- test/sort-images.test.js
+++ test/sort-images.test.js
@@ -4,25 +4,34 @@ const dir = __dirname.split('/')[__dirname.split('/').length - 1];
 const file = dir + __filename.replace(__dirname, '') + ' > ';
 
 test(file + 'Sort a list of images by priority and date upload(sort pririty)', (t) => {
+  t.plan(1);
   const images = [
     {priority: 1, sort: '2015-06-07 13:10:41.0'},
     {priority: 2, sort: '2015-06-07 13:10:41.0'},
     {priority: 1, sort: '2016-06-07 13:10:41.0'},
     {priority: 2, sort: '2015-06-07 16:10:41.0'},
     {priority: 1, sort: '2015-06-07 10:10:40.0'},
-    {priority: 1, sort: '2016-07-07'}
+    {priority: 1, sort: '2016-07-07'},
+    {priority: 1, sort: '2016-07-07'},
+    {position: 1, sort: '2016-07-07'},
+    {position: 2, sort: '2016-07-07'},
+    {position: 1, sort: '2016-07-07'}
   ];
 
   const expected = [
    {priority: 2, sort: '2015-06-07 16:10:41.0'},
    {priority: 2, sort: '2015-06-07 13:10:41.0'},
    {priority: 1, sort: '2016-07-07'},
+   {priority: 1, sort: '2016-07-07'},
+   {position: 1, sort: '2016-07-07'},
+   {position: 1, sort: '2016-07-07'},
+   {position: 2, sort: '2016-07-07'},
    {priority: 1, sort: '2016-06-07 13:10:41.0'},
    {priority: 1, sort: '2015-06-07 13:10:41.0'},
    {priority: 1, sort: '2015-06-07 10:10:40.0'}
   ];
 
-  t.deepEqual(sortImages(images), expected, 'The images are sorted by priority and sort prorierties');
-  t.plan(1);
+  t.deepEqual(sortImages(images), expected, 'The images are sorted by priority, date and position');
   t.end();
 });
+
