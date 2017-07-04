f5c3b03c1bfedb39774a40c1ba1d09ef9f417a0d
Sam Houston <shouston3@me.com>
2017-07-04 11:10:11 +0100

    tests for createDescriptionBox function

--- test/create-description-box.test.js
+++ test/create-description-box.test.js
@@ -1,17 +1,59 @@
 var test = require('tape');
 var createDescriptionBox = require('../lib/create-description-box.js');
 
-/*{ q: { book: true },
-  'page[size]': { '50': true },
-    type: { all: true },
-      categories: { 'Printed Books': true } }*/
+test('createDescriptionBox - with category that doesn\'t have description yet', (t) => {
+  const query = {
+    q: {book: true},
+    'page[size]': {'50': true},
+    type: {all: true},
+    categories: {'Printed Books': true}
+  };
+  const actual = createDescriptionBox(query);
+  const expected = undefined;
 
-/* { q: { phone: true },
- *   'page[size]': { '50': true },
- *     type: { all: true },
- *       categories: { Telecommunications: true } }*/
+  t.equal(actual, expected);
+  t.end();
+});
 
-test('createDescriptionBox', (t) => {
-  t.equal(true, true);
+test('createDescriptionBox - category', (t) => {
+  const query = {
+    q: { phone: true },
+    'page[size]': {'50': true},
+    type: {all: true},
+    categories: {Telecommunications: true}
+  };
+  const actual = Object.keys(createDescriptionBox(query).category);
+  const expected = ['title', 'description', 'sub-categories', 'related-articles'];
+
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('createDescriptionBox - gallery', (t) => {
+  const query = {
+    q: {book: true},
+    'page[size]': {'50': true},
+    type: {all: true},
+    gallery: {'Great Hall': true},
+    museum: {'National Railway Museum': true}
+  };
+  const actual = Object.keys(createDescriptionBox(query).gallery);
+  const expected = ['title', 'thumbnail', 'description', 'link-to-gallery-page'];
+
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('createDescriptionBox - museum', (t) => {
+  const query = {
+    q: {phone: true},
+    'page[size]': {'50': true},
+    type: {all: true},
+    museum: {'Science Museum': true}
+  };
+  const actual = Object.keys(createDescriptionBox(query).museum);
+  const expected = ['title', 'thumbnail', 'description', 'link'];
+
+  t.deepEqual(actual, expected);
   t.end();
 });
