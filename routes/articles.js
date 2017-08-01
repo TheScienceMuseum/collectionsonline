6440e03e619e419f009899d6baf455ca3da3b2de
Jamie Unwin <jamieunwin@gmail.com>
2017-08-01 12:19:15 +0100

    changed related articles to use async

--- routes/articles.js
+++ routes/articles.js
@@ -1,50 +1,36 @@
 var request = require('request');
+var async = require('async');
+
+// Article endpoints on each musuem website
+var endpoints = [
+  { label: 'National Science and Media Museum', url: 'http://www.scienceandmediamuseum.org.uk/collection-media/collection-usage/objects' },
+  { label: 'Museum of Science and Industry', url: 'http://www.msimanchester.org.uk/collection-media/collection-usage/objects' }
+];
 
 module.exports = () => ({
   method: 'GET',
   path: '/articles/{id}',
   config: {
     handler: function (req, reply) {
-      var articles = [];
-      var called = 0;
-
-      // NMeM Bradford
-      request('http://www.scienceandmediamuseum.org.uk/collection-media/collection-usage/objects', (err, res, body) => {
-        called++;
-        if (err) {
-          console.error(err);
-        }
-        var data = JSON.parse(body).filter(e => e.collection_objects.indexOf(req.params.id) > -1);
-        if (data.length) {
-          articles.push({
-            museum: 'National Science and Media Museum',
-            data: data
-          });
-        }
-        complete();
+      async.concat(endpoints, function (endpoint, callback) {
+        request(endpoint.url, (err, res, body) => {
+          var data = JSON.parse(body).filter(e => e.collection_objects.indexOf(req.params.id) > -1);
+          if (err) return callback(err);
+          if (data.length) {
+            return callback(err, {
+              museum: endpoint.label,
+              data: data
+            });
+          }
+          callback();
+        });
+      },
+      function (err, articles) {
+        if (err) { console.error(err); }
+        return reply({ data: articles });
       });
-
-      // MSI Manchester
-      request('http://www.msimanchester.org.uk/collection-media/collection-usage/objects', (err, res, body) => {
-        called++;
-        if (err) {
-          console.error(err);
-        }
-        var data = JSON.parse(body).filter(e => e.collection_objects.indexOf(req.params.id) > -1);
-        if (data.length) {
-          articles.push({
-            museum: 'Museum of Science and Industry',
-            data: data
-          });
-        }
-        complete();
-      });
-
-      function complete () {
-        if (called === 2) {
-          return reply({data: articles});
-        }
-      }
     }
   }
+
 });
+
