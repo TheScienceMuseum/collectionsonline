6c8e322b4109791db7c4891d1c02bb47d9727a92
Jamie Unwin <jamieunwin@gmail.com>
2017-09-25 18:21:00 +0100

    fixes image and thumbnail order for object records

--- test/sort-images.test.js
+++ test/sort-images.test.js
@@ -3,35 +3,41 @@ const sortImages = require('../lib/helpers/jsonapi-response/sort-images');
 const dir = __dirname.split('/')[__dirname.split('/').length - 1];
 const file = dir + __filename.replace(__dirname, '') + ' > ';
 
-test(file + 'Sort a list of images by priority and date upload(sort pririty)', (t) => {
+test(file + 'Sort a list of images by position, priority and date upload(sort pririty)', (t) => {
   t.plan(1);
   const images = [
     {priority: 1, sort: '2015-06-07 13:10:41.0'},
     {priority: 2, sort: '2015-06-07 13:10:41.0'},
     {priority: 1, sort: '2016-06-07 13:10:41.0'},
-    {priority: 2, sort: '2015-06-07 16:10:41.0'},
+    {priority: 2, sort: '2015-05-07 16:10:41.0'},
     {priority: 1, sort: '2015-06-07 10:10:40.0'},
     {priority: 1, sort: '2016-07-07'},
     {priority: 1, sort: '2016-07-07'},
     {position: 1, sort: '2016-07-07'},
+    {position: 1, sort: '2015-07-07'},
     {position: 2, sort: '2016-07-07'},
-    {position: 1, sort: '2016-07-07'}
+    {position: 3, sort: '2016-07-07'},
+    {sort: '2015-06-06'},
+    {sort: '2015-05-05'}
   ];
 
   const expected = [
-   {priority: 2, sort: '2015-06-07 16:10:41.0'},
-   {priority: 2, sort: '2015-06-07 13:10:41.0'},
-   {priority: 1, sort: '2016-07-07'},
-   {priority: 1, sort: '2016-07-07'},
-   {position: 1, sort: '2016-07-07'},
-   {position: 1, sort: '2016-07-07'},
-   {position: 2, sort: '2016-07-07'},
-   {priority: 1, sort: '2016-06-07 13:10:41.0'},
-   {priority: 1, sort: '2015-06-07 13:10:41.0'},
-   {priority: 1, sort: '2015-06-07 10:10:40.0'}
+    { sort: '2015-05-05' },
+    { priority: 2, sort: '2015-05-07 16:10:41.0' },
+    { sort: '2015-06-06' },
+    { priority: 2, sort: '2015-06-07 13:10:41.0' },
+    { priority: 1, sort: '2015-06-07 10:10:40.0' },
+    { priority: 1, sort: '2015-06-07 13:10:41.0' },
+    { position: 1, sort: '2015-07-07' },
+    { priority: 1, sort: '2016-06-07 13:10:41.0' },
+    { priority: 1, sort: '2016-07-07' },
+    { position: 1, sort: '2016-07-07' },
+    { position: 2, sort: '2016-07-07' },
+    { position: 3, sort: '2016-07-07' },
+    { priority: 1, sort: '2016-07-07' }
   ];
 
-  t.deepEqual(sortImages(images), expected, 'The images are sorted by priority, date and position');
+  t.deepEqual(sortImages(images), expected, 'The images are sorted by position, date and priority');
   t.end();
 });
 
