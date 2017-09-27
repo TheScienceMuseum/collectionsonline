f7fec571e75f8d7db0611c6ac12c521c46f7234c
Danielwhyte <danwhy@gmail.com>
2017-09-27 12:16:55 +0100

    uncomments nw tests #1023

--- test/client/back.clienttest.js
+++ test/client/back.clienttest.js
@@ -1,6 +1,5 @@
 
 module.exports = {
-/* commented out due to selenium issues / add back 16:9:2017 JU
   'Search from home Page': function (browser) {
     browser
     .url('http://localhost:8000/search?q=book')
@@ -8,11 +7,11 @@ module.exports = {
     .click('.resultcard')
     .waitForElementVisible('.searchbox__back', 1000)
     .assert.attributeEquals('.searchbox__back', 'href', 'http://localhost:8000/search?q=book')
+    .pause(1000)
     .url('http://localhost:8000/objects/co62243')
     .waitForElementVisible('body', 1000)
     .waitForElementVisible('.searchbox__back', 1000)
     .assert.attributeEquals('.searchbox__back', 'href', 'http://localhost:8000/')
     .end();
   }
-*/
 };
