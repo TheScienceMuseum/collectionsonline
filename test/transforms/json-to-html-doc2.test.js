4d2668cfa22c956ad2003ad31a63e389f037aeb7
Jamie Unwin <jamieunwin@gmail.com>
2017-09-29 16:31:57 +0100

    add test for subject on documents

---
+++ test/transforms/json-to-html-doc2.test.js
@@ -0,0 +1,24 @@
+const test = require('tape');
+const config = require('../../config');
+const buildJSONResponse = require('../../lib/jsonapi-response');
+const JSONAPIResponse = buildJSONResponse(require('../fixtures/elastic-responses/example-get-response-document2.json'), config);
+const buildHTMLData = require('../../lib/transforms/json-to-html-data');
+var HTMLData;
+
+test('HTMLData should be transformed succesfully', (t) => {
+  t.plan(1);
+  t.doesNotThrow(() => {
+    HTMLData = buildHTMLData(JSONAPIResponse);
+  }, 'Transform did not throw error');
+  t.end();
+});
+
+test('Data fields should have correct values', function (t) {
+  console.log(HTMLData.details);
+  t.doesNotThrow(() => {
+    HTMLData = buildHTMLData(JSONAPIResponse);
+  }, 'Transform did not throw error');
+  t.equal(HTMLData.title, 'Works photographic negative of cable laying, Ebbw Vale', 'title should be correct');
+  t.ok(HTMLData.details.find(e => e.key === 'Subject'), 'subject should exist');
+  t.end();
+});
