3da5e4498e5da3ebde377006f51aa3ed04e2578f
Jamie Unwin <jamieunwin@gmail.com>
2017-11-20 11:44:40 +0000

    reduce paginatin to 10 pages

--- schemas/search.js
+++ schemas/search.js
@@ -4,7 +4,7 @@ module.exports = {
   query: {
     q: Joi.string().allow(''),
     'page[number]': Joi.number().integer().min(0).max(300),
-    'page[size]': Joi.number().integer().min(1).max(100),
+    'page[size]': Joi.number().integer().min(1).max(10),
     'page[type]': Joi.string().valid('search', 'results-list'),
     'ajax': Joi.boolean().truthy('true').falsy('false'),
     'random': Joi.number().integer().min(1).max(50)
