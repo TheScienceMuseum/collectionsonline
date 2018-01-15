9b4c9354af8e78596458b6d550cdb01b6461827b
Jamie Unwin <jamieunwin@gmail.com>
2018-01-15 12:03:43 +0000

    update what is? questions

--- fixtures/whatis.js
+++ fixtures/whatis.js
@@ -6,20 +6,40 @@ module.exports = {
     id: 'co8015917',
     type: 'object',
     attributes: {
-      summary_title: 'What is the smallest object?'
+      summary_title: 'What is the smallest object in the collection?'
     },
     links: {
-      self: '/objects/co8015917'
+      self: '/objects/co8015917/microscopic-view-of-a-flea-trade-card'
     }
   },
   {
-    id: 'co8448331',
+    id: 'co29806',
     type: 'objects',
     attributes: {
-      summary_title: 'What is the largest object?'
+      summary_title: 'What is the largest object in the collection?'
     },
     links: {
-      self: '/objects/co8448331'
+      self: '/objects/co29806/concorde-002-aircraft-aeroplanes-airliners-passenger-aircraft-prototypes'
+    }
+  },
+  {
+    id: 'co8593265',
+    type: 'objects',
+    attributes: {
+      summary_title: 'What is the newest object in the collection?'
+    },
+    links: {
+      self: '/objects/co8593265/soyuz-tma-19m-descent-module-s-p-korolev-rocket-and-space-public-corporation-energia'
+    }
+  },
+  {
+    id: 'co45638',
+    type: 'objects',
+    attributes: {
+      summary_title: 'What is the oldest object in the collection?'
+    },
+    links: {
+      self: '/objects/co45638/paleolithic-flint-hand-axe-found-at-northfleet'
     }
   }]
 };
