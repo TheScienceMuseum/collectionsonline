389a83fa88eec1ba90d15725e229ace38a02523f
Jamie Unwin <jamieunwin@jamies-mbp.ad.nmsi.ac.uk>
2018-08-23 15:15:33 +0100

    Adds hocks into index for 360s

--- routes/embed.js
+++ routes/embed.js
@@ -1,17 +1,36 @@
-var getRotational = require('../lib/get-rotational');
+const Boom = require('boom');
+const buildJSONResponse = require('../lib/jsonapi-response');
+const TypeMapping = require('../lib/type-mapping');
 
 module.exports = {
-  rotational () {
+  rotational (elastic, config) {
     return {
       method: 'GET',
       path: '/embed/rotational/{coid}',
       config: {
         handler: function (request, reply) {
-          return reply.view(
-            'rotational',
-            { configurl: getRotational(request.params.coid) },
-            { layout: 'embed' }
-          );
+          elastic.get({index: 'smg', type: 'object', id: TypeMapping.toInternal(request.params.coid)}, (err, result) => {
+            if (err) {
+              if (err.status === 404) {
+                return reply(Boom.notFound());
+              }
+              return reply(Boom.serverUnavailable('unavailable'));
+            }
+            var res = buildJSONResponse(result, config);
+            if (res.data.attributes.enhancement) {
+              res.data.attributes.enhancement.web.find((el) => {
+                // if (el.platform === 'rotational') {
+                var rid = 'smgco-360/28f0ba52-6deb-43c0-a2e5-1fb82c5247cd'; // dummy test data
+                var configUrl = 'https://s3-eu-west-1.amazonaws.com/' + rid + '/object.xml';
+                return reply.view(
+                    'rotational',
+                    { configurl: configUrl },
+                    { layout: 'embed' }
+                );
+                // }
+              });
+            }
+          });
         }
       }
     };
