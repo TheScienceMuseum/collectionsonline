0230d70f12b46a7575f6bd74516382fe343a3c0a
SimonLab <simon.labondance@gmail.com>
2018-11-06 18:58:04 +0000

    server rendering home and about page, https://github.com/TheScienceMuseum/collectionsonline/issues/1194#issuecomment-436366415

--- client/routes/home.js
+++ client/routes/home.js
@@ -1,30 +1,3 @@
-var Templates = require('../templates');
-
-var data = require('../../fixtures/data');
-
-var searchListener = require('../lib/listeners/search-listener');
-var initComp = require('../lib/listeners/init-components');
-
 module.exports = function (page) {
-  page('/', render, listeners);
-
-  function render (ctx, next) {
-    if (!ctx.isInitialRender) {
-      var pageEl = document.getElementsByTagName('main')[0];
-      data.footer = require('../../fixtures/footer');
-      data.museums = require('../../fixtures/museums');
-      pageEl.innerHTML = Templates['home'](data);
-      // refresh the title of the page
-      document.getElementsByTagName('title')[0].textContent = data.titlePage;
-      document.body.className = ctx.state.data.type;
-      next();
-    } else {
-      listeners();
-    }
-  }
-
-  function listeners (ctx, next) {
-    searchListener();
-    initComp();
-  }
+  page('/');
 };
