5f6f659092bb8004bffeecd1d2eac3d85b3d7798
Danielwhyte <danwhy@gmail.com>
2019-03-14 10:58:21 +0000

    refactors search for hapi 17/18

--- routes/route-helpers/response.js
+++ routes/route-helpers/response.js
@@ -1,15 +1,15 @@
 const JSONToHTML = require('../../lib/transforms/json-to-html-data.js');
 
-module.exports = function (reply, data, type, responseType) {
+module.exports = function (h, data, type, responseType) {
   if (responseType === 'html') {
     const pageData = {
       page: type
     };
     const HTMLData = JSONToHTML(data);
-    return reply.view(type, Object.assign(HTMLData, pageData));
+    return h.view(type, Object.assign(HTMLData, pageData));
   }
 
   if (responseType === 'json') {
-    return reply(data).header('content-type', 'application/vnd.api+json');
+    return h.response(data).header('content-type', 'application/vnd.api+json');
   }
 };
