cc20480c8e67971525c07c471d777f738bcd1eb8
Jamie Unwin <jamieunwin@gmail.com>
2019-11-04 14:11:39 +0000

    Adds Wordpress collection feeds

--- routes/articles.js
+++ routes/articles.js
@@ -6,7 +6,12 @@ var endpoints = [
   { label: 'National Science and Media Museum', url: 'https://www.scienceandmediamuseum.org.uk/collection-media/collection-usage/objects' },
   { label: 'Science and Industry Musem', url: 'https://www.scienceandindustrymuseum.org.uk/collection-media/collection-usage/objects' },
   { label: 'Science Museum', url: 'https://www.sciencemuseum.org.uk/collection-media/collection-usage/objects' },
-  { label: 'Railway Museum', url: 'https://www.railwaymuseum.org.uk/collection-media/collection-usage/objects' }
+  { label: 'Railway Museum', url: 'https://www.railwaymuseum.org.uk/collection-media/collection-usage/objects' },
+  { label: 'Learning Resources', url: 'https://learning-resources.sciencemuseum.org.uk/wp-json/collection-media/collection-usage' },
+  { label: 'Science Musuem Blog', url: 'https://blog.sciencemuseum.org.uk/wp-json/collection-media/collection-usage' },
+  { label: 'Science and Media Museum Blog', url: 'https://blog.scienceandmediamuseum.org.uk/wp-json/collection-media/collection-usage' },
+  { label: 'Science and Industry Museum Blog', url: 'https://blog.scienceandindustrymuseum.org.uk/wp-json/collection-media/collection-usage' },
+  { label: 'Railway Musuem Blog', url: 'https://blog.railwaymuseum.org.uk/wp-json/collection-media/collection-usage' }
 ];
 
 module.exports = () => ({
