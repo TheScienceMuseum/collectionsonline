94b6a30e3de504253835093c4947f29e636a5555
Jamie Unwin <jamieunwin@gmail.com>
2020-01-27 13:58:59 +0000

    Add user agent to article feed parser

--- routes/articles.js
+++ routes/articles.js
@@ -19,11 +19,10 @@ module.exports = (config) => ({
   method: 'GET',
   path: '/articles/{id}',
   config: {
-    cache: cacheHeaders(config, 3600 * 24),
+    cache: cacheHeaders(config, 3600 * 12),
     handler: async function (req, h) {
       try {
         const articles = await Promise.all(endpoints.map(e => fetchArticles(e, req.params.id)));
-
         return h.response({ data: articles.filter(e => e.data.length > 0) });
       } catch (err) {
         return new Boom('Cannot parse related objects feed:', err);
@@ -34,7 +33,11 @@ module.exports = (config) => ({
 
 async function fetchArticles (endpoint, id) {
   try {
-    const response = await axios.get(endpoint.url);
+    const aclient = axios.create({
+      timeout: 10000,
+      headers: { 'User-Agent': 'SMG Collection Site 1.0' }
+    });
+    const response = await aclient.get(endpoint.url);
     const data = response.data ? response.data.filter(e => e.collection_objects.indexOf(id) > -1) : [];
 
     return {
@@ -42,6 +45,7 @@ async function fetchArticles (endpoint, id) {
       data: data
     };
   } catch (err) {
+    console.log(err);
     throw err;
   }
 }
