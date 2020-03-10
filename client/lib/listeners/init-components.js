fdef8e7dbf7a7c8fc658b806488faac5bf79bfa4
Toby Stokes <toby@tobystokes.uk>
2020-03-10 15:26:45 +0000

    add new group navigation from WDS
    and refactor a few things to match other sites the WDS way

--- client/lib/listeners/init-components.js
+++ client/lib/listeners/init-components.js
@@ -1,16 +1,16 @@
-var svg4everybody = require("svg4everybody");
-var searchBox = require("./search-box");
-var clipboard = require("./clipboard");
-var map = require("./map");
-var moreButton = require("./more-button");
-var moreCordion = require("./accordion");
-var carousel = require("./carousel");
-var moreDetails = require("./expand-details-button");
-var audioplayer = require("./audioplayer");
-var mediaplayer = require("./mediaplayer");
-var headerMenu = require("./header-menu");
-var illuminate = require("./illuminate");
-var osd = require("./osd");
+var svg4everybody = require('svg4everybody');
+var searchBox = require('./search-box');
+var clipboard = require('./clipboard');
+var map = require('./map');
+var moreButton = require('./more-button');
+var moreCordion = require('smg-web-design-system/dist/js/accordion');
+var carousel = require('./carousel');
+var moreDetails = require('./expand-details-button');
+var audioplayer = require('smg-web-design-system/dist/js/audioplayer');
+var mediaplayer = require('./mediaplayer');
+var headerMenu = require('smg-web-design-system/dist/js/menu');
+var illuminate = require('smg-web-design-system/dist/js/illuminate');
+var osd = require('./osd');
 
 module.exports = ctx => {
   svg4everybody();
--- client/lib/loading-bar.js
+++ client/lib/loading-bar.js
@@ -1,17 +1,17 @@
-var loadingEl = function() {
+var loadingEl = function () {
   // Attach the loading bar to a different element if on the home page
   return (
-    document.querySelector(".home-banner") ||
-    document.querySelector(".searchbar")
+    document.querySelector('.home-banner') ||
+    document.querySelector('.searchbar')
   );
 };
 
 module.exports = {
-  start: function() {
-    loadingEl().classList.add("c-loading");
+  start: function () {
+    loadingEl().classList.add('c-loading');
   },
 
-  end: function() {
-    loadingEl().classList.remove("c-loading");
+  end: function () {
+    loadingEl().classList.remove('c-loading');
   }
 };
