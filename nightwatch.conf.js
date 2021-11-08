b6ae0090289c9f3716b5ff460af613f1c4081ff3
Jamie Unwin <jamieunwin@gmail.com>
2021-11-08 12:28:05 +0000

    update selenium download funtion

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -89,9 +89,9 @@ module.exports = {
 
 fs.stat(binpath + 'selenium.jar', function (err, stat) {
   if (err || !stat || stat.size < 1) {
-    // require('selenium-download').ensure(binpath, function (error) {
-    //   if (error) throw new Error(error);
-    //   console.log('✔ Selenium & Chromedriver downloaded to:', binpath);
-    // });
+    require('selenium-download').ensure(binpath, function (error) {
+      if (error) throw new Error(error);
+      console.log('✔ Selenium & Chromedriver downloaded to:', binpath);
+    });
   }
 });
