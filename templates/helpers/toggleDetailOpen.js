00cea03a55fb9b0bce1855b3b6794c7f9f1317dc
Jamie Unwin <jamieunwin@gmail.com>
2023-04-03 16:08:30 +0100

    Fix measurements on archive documents

--- templates/helpers/toggleDetailOpen.js
+++ templates/helpers/toggleDetailOpen.js
@@ -2,6 +2,5 @@
 
 module.exports = function (key, options) {
   var toggleFor = ['TRANSCRIPTION'];
-  console.log("***" + options.data.root.page);
   return toggleFor.indexOf(key.toUpperCase()) > -1 && options.data.root.page === 'archive';
 };
