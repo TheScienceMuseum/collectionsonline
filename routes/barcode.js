fc695a205ab6c63b668527652241aaee2ce0f4e6
Jamie Unwin <jamieunwin@gmail.com>
2024-02-02 13:41:33 +0000

    Fixes semistandard errors  in barcode scanning

--- routes/barcode.js
+++ routes/barcode.js
@@ -1,4 +1,4 @@
-// const slug = require('slugg');
+const slug = require('slugg');
 
 module.exports = (elastic, config) => ({
   method: 'GET',
@@ -7,27 +7,23 @@ module.exports = (elastic, config) => ({
     handler: async function (request, h) {
       if (request.params.uid) {
         const barcode = escape(request.params.uid);
-        var body = {
+        const body = {
           query: {
-            "bool": {
-                "must": [{
-                    "match": {
-                        "barcode.value": barcode
-                    }
-                }]
+            bool: {
+              must: [{ match: { 'barcode.value': barcode } }]
             }
           }
         };
         try {
-          const result = await elastic.search({ index: 'ciim', body: body });
+          const result = await elastic.search({ index: 'ciim', body });
           if (result.body.hits.hits) {
-            var obj = result.body.hits.hits[0];
-            var slugValue = obj._source.summary_title && slug(obj._source.summary_title).toLowerCase();
+            const obj = result.body.hits.hits[0];
+            let slugValue = obj._source.summary_title && slug(obj._source.summary_title).toLowerCase();
             slugValue = slugValue ? ('/' + slugValue) : '';
-            path = '/objects/' + obj._id + slugValue;
+            const path = '/objects/' + obj._id + slugValue;
             return h.redirect(config.rootUrl + path).permanent();
           } else {
-            return "Barcode not found";
+            return 'Barcode not found';
           }
         } catch (err) {
           return 'Error: ' + err;
