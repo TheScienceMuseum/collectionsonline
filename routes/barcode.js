7343be4c0e21db6033ff5ec92229fb8a8a89bb9a
Jamie Unwin <jamieunwin@gmail.com>
2024-02-02 13:28:43 +0000

    Plumbs in barcode scanning

--- routes/barcode.js
+++ routes/barcode.js
@@ -7,26 +7,31 @@ module.exports = (elastic, config) => ({
     handler: async function (request, h) {
       if (request.params.uid) {
         const barcode = escape(request.params.uid);
-        return `Barcode found: <b>${barcode}</b><br>Barcode not found in index<br><a href="/barcode">Scan again</a>`;
-        // var body = {
-        //   query: {
-        //     bool: { filter: { term: { 'identifier.value': barcode } } }
-        //   }
-        // };
-        // try {
-        //   const result = await elastic.search({ index: 'ciim', body: body });
-        //   if (result.hits.total>0) {
-        //     var obj = result.hits.hits[0];
-        //     var slugValue = obj._source.summary_title && slug(obj._source.summary_title).toLowerCase();
-        //     slugValue = slugValue ? ('/' + slugValue) : '';
-        //     path = '/objects/' + obj._id + slugValue;
-        //     return h.redirect(config.rootUrl + path).permanent();
-        //   } else {
-        //     return "Barcode not found";
-        //   }
-        // } catch (err) {
-        //   return 'Error: ' + err;
-        // }
+        var body = {
+          query: {
+            "bool": {
+                "must": [{
+                    "match": {
+                        "barcode.value": barcode
+                    }
+                }]
+            }
+          }
+        };
+        try {
+          const result = await elastic.search({ index: 'ciim', body: body });
+          if (result.body.hits.hits) {
+            var obj = result.body.hits.hits[0];
+            var slugValue = obj._source.summary_title && slug(obj._source.summary_title).toLowerCase();
+            slugValue = slugValue ? ('/' + slugValue) : '';
+            path = '/objects/' + obj._id + slugValue;
+            return h.redirect(config.rootUrl + path).permanent();
+          } else {
+            return "Barcode not found";
+          }
+        } catch (err) {
+          return 'Error: ' + err;
+        }
       } else {
         return h.view('barcode');
       }
