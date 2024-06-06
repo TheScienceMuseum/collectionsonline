ffc73adb967bcfa26dbb3c322fa982c1de3db881
Toby Stokes <toby@tobystokes.uk>
2024-06-06 12:25:54 +0100

    replace all pre-html details togglers

--- client/lib/listeners/more-button.js
+++ client/lib/listeners/more-button.js
@@ -1,10 +1,12 @@
 module.exports = function () {
-  const body = document.querySelector('body');
-  body.addEventListener('click', function (e) {
-    if (e.target && e.target.className === 'details__summary') {
-      const parentElement = e.target.parentElement;
-      const attributeValue = parentElement.getAttribute('aria-expanded') !== 'true';
-      parentElement.setAttribute('aria-expanded', attributeValue);
-    }
+  const details = document.querySelectorAll('details');
+  details.forEach((el) => {
+    el.addEventListener('toggle', function (e) {
+      if (e.target && e.target.className === 'c-truncated') {
+        const summary = e.target.querySelector('summary');
+        summary.innerHTML = e.target.hasAttribute("open") ? 'Less' : 'More';
+      }
+    });
   });
+  
 };
