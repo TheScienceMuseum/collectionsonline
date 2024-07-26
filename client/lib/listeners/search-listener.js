78696659b1cba54f61673da4c775f8fa43b386a8
Toby Stokes <toby@tobystokes.uk>
2024-07-26 13:14:11 +0100

    semistandard fix

--- client/lib/listeners/search-listener.js
+++ client/lib/listeners/search-listener.js
@@ -11,63 +11,65 @@ const keyCategories = require('../../../fixtures/key-categories.js');
 
 module.exports = function () {
   const searchBoxEl = document.getElementById('searchbox');
-  if (searchBoxEl) searchBoxEl.addEventListener('submit', function (e) {
-    loadingBar.start();
-    e.preventDefault();
-    const searchValue = document.querySelector('.searchbox__search').value;
-    const categoryFilter = document.querySelector('.searchbox__category__filter') && document.querySelector('.searchbox__category__filter').value;
-    const collectionFilter = document.querySelector('.searchbox__collection__filter') && document.querySelector('.searchbox__collection__filter').value;
-    const museumFilter = document.querySelector('.searchbox__museum__filter') && document.querySelector('.searchbox__museum__filter').value;
-    const galleryFilter = document.querySelector('.searchbox__gallery__filter') && document.querySelector('.searchbox__gallery__filter').value;
-    const archiveFilter = document.querySelector('.searchbox__archive__filter') && document.querySelector('.searchbox__archive__filter').value;
-    const hasImageFilter = document.querySelector('.searchbox__hasImage__filter') && document.querySelector('.searchbox__hasImage__filter').value;
-    const imageLicenseFilter = document.querySelector('.searchbox__imageLicense__filter') && document.querySelector('.searchbox__imageLicense__filter').value;
-    const q = searchValue || null;
-    const qs = { q };
+  if (searchBoxEl) {
+    searchBoxEl.addEventListener('submit', function (e) {
+      loadingBar.start();
+      e.preventDefault();
+      const searchValue = document.querySelector('.searchbox__search').value;
+      const categoryFilter = document.querySelector('.searchbox__category__filter') && document.querySelector('.searchbox__category__filter').value;
+      const collectionFilter = document.querySelector('.searchbox__collection__filter') && document.querySelector('.searchbox__collection__filter').value;
+      const museumFilter = document.querySelector('.searchbox__museum__filter') && document.querySelector('.searchbox__museum__filter').value;
+      const galleryFilter = document.querySelector('.searchbox__gallery__filter') && document.querySelector('.searchbox__gallery__filter').value;
+      const archiveFilter = document.querySelector('.searchbox__archive__filter') && document.querySelector('.searchbox__archive__filter').value;
+      const hasImageFilter = document.querySelector('.searchbox__hasImage__filter') && document.querySelector('.searchbox__hasImage__filter').value;
+      const imageLicenseFilter = document.querySelector('.searchbox__imageLicense__filter') && document.querySelector('.searchbox__imageLicense__filter').value;
+      const q = searchValue || null;
+      const qs = { q };
 
-    if (categoryFilter) {
-      qs['filter[categories]'] = categoryFilter;
-    }
-    if (collectionFilter) {
-      qs['filter[collection]'] = collectionFilter;
-    }
-    if (museumFilter) {
-      qs['filter[museum]'] = museumFilter;
-    }
-    if (galleryFilter) {
-      qs['filter[gallery]'] = galleryFilter;
-    }
-    if (archiveFilter) {
-      qs['filter[archive]'] = archiveFilter;
-    }
-    if (hasImageFilter) {
-      qs['filter[has_image]'] = hasImageFilter;
-    }
-    if (imageLicenseFilter) {
-      qs['filter[image_license]'] = imageLicenseFilter;
-    }
-
-    if (!qs['filter[categories]'] && qs.q) {
-      const lq = qs.q.toLowerCase();
-      let qMatch;
-
-      if (keyCategories.some(el => {
-        if (el.category === lq || el.synonyms.indexOf(lq) > -1) {
-          qMatch = el.category;
-          return true;
-        } else {
-          return false;
-        }
-      })) {
-        qs['filter[categories]'] = qMatch;
-        qs.q = '';
+      if (categoryFilter) {
+        qs['filter[categories]'] = categoryFilter;
       }
-    }
+      if (collectionFilter) {
+        qs['filter[collection]'] = collectionFilter;
+      }
+      if (museumFilter) {
+        qs['filter[museum]'] = museumFilter;
+      }
+      if (galleryFilter) {
+        qs['filter[gallery]'] = galleryFilter;
+      }
+      if (archiveFilter) {
+        qs['filter[archive]'] = archiveFilter;
+      }
+      if (hasImageFilter) {
+        qs['filter[has_image]'] = hasImageFilter;
+      }
+      if (imageLicenseFilter) {
+        qs['filter[image_license]'] = imageLicenseFilter;
+      }
 
-    const params = paramify(qs);
-    const query = querify(qs);
+      if (!qs['filter[categories]'] && qs.q) {
+        const lq = qs.q.toLowerCase();
+        let qMatch;
 
-    const url = '/search' + params + query;
-    page.show(url.split(' ').join('-'));
-  });
+        if (keyCategories.some(el => {
+          if (el.category === lq || el.synonyms.indexOf(lq) > -1) {
+            qMatch = el.category;
+            return true;
+          } else {
+            return false;
+          }
+        })) {
+          qs['filter[categories]'] = qMatch;
+          qs.q = '';
+        }
+      }
+
+      const params = paramify(qs);
+      const query = querify(qs);
+
+      const url = '/search' + params + query;
+      page.show(url.split(' ').join('-'));
+    });
+  }
 };
