7a5f3dc18eb83fcf0c6c1afcaa7058b86ba81ebd
Evan Parker <65721884+evancp87@users.noreply.github.com>
2024-09-17 17:54:52 +0100

    feat: barcode react app first commit

--- routes/barcode.js
+++ routes/barcode.js
@@ -6,22 +6,29 @@ module.exports = (elastic, config) => ({
   config: {
     handler: async function (request, h) {
       if (request.params.uid && request.params.uid.match(/^[A-Za-z0-9]*$/)) {
-        const barcode = escape(request.params.uid);
+        const barcode = encodeURIComponent(request.params.uid);
         const body = {
           query: {
             bool: {
-              must: [{ match: { 'barcode.value': barcode } }]
-            }
-          }
+              must: [{ match: { 'barcode.value': barcode } }],
+            },
+          },
         };
         try {
           const result = await elastic.search({ index: 'ciim', body });
           if (result.body.hits.hits) {
             const obj = result.body.hits.hits[0];
-            let slugValue = obj._source.summary_title && slug(obj._source.summary_title).toLowerCase();
-            slugValue = slugValue ? ('/' + slugValue) : '';
+            let slugValue =
+              obj._source.summary_title &&
+              slug(obj._source.summary_title).toLowerCase();
+            const title = obj._source?.summary?.title;
+            const image =
+              config.mediaPath +
+              obj._source?.multimedia?.[0]?.['@processed']?.medium?.location;
+            const uid = obj?._id;
+            slugValue = slugValue ? '/' + slugValue : '';
             const path = '/objects/' + obj._id + slugValue;
-            return h.redirect(config.rootUrl + path).permanent();
+            return h.response({ path, title, image, uid });
           } else {
             return 'Barcode not found';
           }
@@ -29,8 +36,8 @@ module.exports = (elastic, config) => ({
           return 'Error: ' + err;
         }
       } else {
-        return h.view('barcode', null, { layout: 'basic' });
+        return h.view('barcode', {}, { layout: 'basic' });
       }
-    }
-  }
+    },
+  },
 });
