05aa6ecf8ef3ed0cdbc05f1f54dcba3679327d61
Jamie Unwin <jamieunwin@gmail.com>
2024-10-03 12:49:26 +0100

    Add fix for no title parts and update masthead logic on groups

--- templates/helpers/sphDescription.js
+++ templates/helpers/sphDescription.js
@@ -1,9 +1,10 @@
 module.exports = (description, part) => {
   // for sph records that have no primary.initialDescription, make a summary from the web.initialDescription, and use the rest as moreDescription
-
-  if (part === 'initialDescription') {
-    return description.primary?.initialDescription || description.web?.initialDescription.slice(0, 1);
-  } else if (part === 'moreDescription') {
-    return description.primary?.initialDescription ? description.web?.initialDescription : description.web?.initialDescription.slice(1);
+  if (description) {
+    if (part === 'initialDescription') {
+      return description.primary?.initialDescription || description.web?.initialDescription.slice(0, 1);
+    } else if (part === 'moreDescription') {
+      return description.primary?.initialDescription ? description.web?.initialDescription : description.web?.initialDescription.slice(1);
+    }
   }
 };
