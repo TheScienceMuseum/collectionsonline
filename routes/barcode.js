272ec2156772559bf3a3d7488046469052769ce2
Jamie Unwin <jamieunwin@gmail.com>
2024-10-17 16:41:56 +0100

    use basic handlbars layout and set title for barcode app

--- routes/barcode.js
+++ routes/barcode.js
@@ -49,7 +49,7 @@ module.exports = (elastic, config) => ({
           return 'Error: ' + err;
         }
       } else {
-        return h.view('barcode', {}, { layout: 'default' });
+        return h.view('barcode', { 'titlePage': 'SMG Barcode Scanner' }, { layout: 'basic' });
       }
     }
   }
