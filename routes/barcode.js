42ad85bbec8ef410e2efbb31623224c653e2f02e
Evan Parker <65721884+evancp87@users.noreply.github.com>
2024-12-09 16:51:05 +0000

    chore: comments for barcode app

--- routes/barcode.js
+++ routes/barcode.js
@@ -11,42 +11,52 @@ module.exports = (elastic, config) => ({
         const body = {
           query: {
             bool: {
-              must: [{ match: { 'barcode.value': barcode } }]
-            }
-          }
+              must: [{ match: { 'barcode.value': barcode } }],
+            },
+          },
         };
         try {
           const result = await elastic.search({ index: 'ciim', body });
+
           if (result.body.hits.hits && result.body.hits.hits.length) {
             const obj = result.body.hits.hits[0];
+
             if (!obj) {
               return h.redirect(config.rootUrl + '/barcode').permanent();
             }
+
             let slugValue =
               obj?._source?.summary_title &&
               slug(obj?._source?.summary_title).toLowerCase();
+
             const title = obj?._source?.summary?.title;
+
             const image =
               config.mediaPath +
               obj?._source?.multimedia?.[0]?.['@processed']?.medium?.location;
+
             const uid = obj?._id;
             slugValue = slugValue ? '/' + slugValue : '';
+
             const path = '/objects/' + obj?._id + slugValue;
+
             const description = obj?._source?.description?.[0]?.value;
+
             const barcodeId = obj?._source?.barcode?.value;
+
             return h.response({
               path,
               title,
               image,
               uid,
               description,
-              barcodeId
+              barcodeId,
             });
           } else {
             return h
               .response({
                 error:
-                  'Either an invalid barcode was used, or that item has not yet been added to the database'
+                  'Either an invalid barcode was used, or that item has not yet been added to the database',
               })
               .code(404)
               .type('application/json');
@@ -57,6 +67,6 @@ module.exports = (elastic, config) => ({
       } else {
         return h.view('barcode', {}, { layout: 'basic' });
       }
-    }
-  }
+    },
+  },
 });
