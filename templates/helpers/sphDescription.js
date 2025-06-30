5f33b049bdd06685a638e70da7e74b1cf5fad244
Jamie Unwin <jamieunwin@gmail.com>
2025-06-30 13:15:45 +0100

    fix sph descriptions

--- templates/helpers/sphDescription.js
+++ templates/helpers/sphDescription.js
@@ -1,12 +1,12 @@
 module.exports = (description, part) => {
-  console.log(description);
-  console.log(part);
+  // console.log(description);
+  // console.log(part);
   // for sph records that have no primary.initialDescription, make a summary from the web.initialDescription, and use the rest as moreDescription
   if (description) {
     if (part === 'initialDescription') {
       return description.primary?.initialDescription || description.web?.initialDescription.slice(0, 1);
     } else if (part === 'moreDescription') {
-      return description.primary?.initialDescription ? description.initialDescription : description.web?.initialDescription.slice(1);
+      return description.primary?.initialDescription ? description.web?.initialDescription : description.web?.initialDescription.slice(1);
     }
   }
 };
